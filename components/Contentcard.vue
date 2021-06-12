bcd04a797a1556f6fb642533c5f73bee409b6148
Prabhjeev Nijjar <prabhjeev@so.city>
2021-06-12 20:59:49 +0530

    Upvote downvote feature added

--- components/Contentcard.vue
+++ components/Contentcard.vue
@@ -8,21 +8,21 @@
             <a :href="item.resourcelink">click here to access</a>
           </div>
           <div class="contentcard_tags">
-            #{{item.resourcetype}} #{{item.resourcesubtype["item"]}} 
+            #{{item.resourcetype}} 
           </div>        
           <div class="contentcard_socials">
             <div class="contentcard_socials_like">
-              <a>Upvote</a>
+              <button v-on:click="upVote(item._id)" type="submit">Upvote</button>
               <a>{{item.upvotecount}}</a>
             </div>
             <div class="contentcard_socials_dislike">
-              <a>Downvote</a>
+              <button v-on:click="downVote(item._id)" type="submit">Downvote</button>
               <a>{{item.downvotecount}}</a>
             </div>
             <div class="contentcard_socials_bookmark">
-              <input type="button" value="save">
+              <button type="submit">Save</button>
             </div>
-            <div>{{serverResponse.data["0"]}}</div>
+            <!-- <div>{{serverResponse.data["0"]}}</div> -->
           </div>
           <hr>
       </div>
@@ -46,7 +46,28 @@ export default {
       }) 
   },
   methods: {
-      async upVote()
+      async upVote(postId) {
+       console.log(postId);
+        await this.$axios.$put('http://localhost:3001/homepage/up-vote',{body:{"null":"null"}},{ params: {adId: postId }})
+          .then((serverResponse)=>{
+            this.serverResponse = serverResponse;
+            console.log("res data: ",serverResponse);
+          })
+          .catch((error)=>{
+            console.log("error: ",error);
+          }) 
+      },
+      async downVote(postId) {
+        console.log(postId);
+        this.$axios.$put('http://localhost:3001/homepage/down-vote',{body:{"null":"null"}},{ params: { adId: postId } })
+          .then((serverResponse)=>{
+            this.serverResponse = serverResponse;
+            console.log("res data: ",serverResponse);
+          })
+          .catch((error)=>{
+            console.log("error: ",error);
+          }) 
+      }
   }
 }
 </script>
@@ -93,6 +114,22 @@ export default {
 }
 .contentcard_socials_dislike{
   margin-right:5px;
+}
+.contentcard_socials_like button {
+    border: none;
+    background-color: pink;
+    border-radius: 6px;
+}
+.contentcard_socials_dislike button {
+  border: none;
+  background-color: rgb(252, 209, 216);
+  border-radius: 6px;
+}
+.contentcard_socials_bookmark button {
+  border: none;
+  background-color: #50a5fa;
+  color: aliceblue;
+  border-radius: 6px;
 }
 .contentcard_socials_bookmark{
   margin-right:5px;
